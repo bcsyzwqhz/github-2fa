3a57420e0b0213753037dbcf511405c0c8a7930b
bcsyzwqhz <makerlai@163.com>
2024-07-15 20:34:30 +0800

    添加文件认证

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyotp
 import pyperclip as cp
-secret_key = 'your_2fa_token'
+with open('2fa-token.txt','r') as file:
+    secret_key = file.read()
 cp.copy(pyotp.TOTP(secret_key).now())
 print(cp.paste())
